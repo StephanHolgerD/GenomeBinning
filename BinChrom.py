ccebd721d97c1a89da3ea183e8c002c5073d35ac
StephanHolgerD <stephdruk@googlemail.com>
2022-02-06 12:19:54 +0100

    removed tagStr, align start, end

--- BinChrom.py
+++ BinChrom.py
@@ -3,8 +3,8 @@ import pysam
 import sys
 from multiprocessing import Pool
 import argparse
-
-
+import numpy as np
+from tqdm import tqdm
 
 #!/usr/bin/env python3
 
@@ -15,10 +15,10 @@ requiredNamed.add_argument('-o','--out', help='out file', required=True)
 
 optArguments = parser.add_argument_group('optional arguments')
 optArguments.add_argument('-c','--chromosome', help='name of chromosome/contig', required=False,default='all')
-optArguments.add_argument('-s','--binsize', help='size of bins', required=False,default=5000)
-optArguments.add_argument('--threads',default=1, help="number of cpu's  to run in paralell, ROI <1000 will always use 1 core",type=int)
-optArguments.add_argument('--tagKey',default=None, help="key for tag of interest",type=str)
-optArguments.add_argument('--tagString',default=None, help="string contained in tag of interest",type=str)
+optArguments.add_argument('-s','--binsize', help='size of bins', required=False,default=5000, type=int)
+optArguments.add_argument('-p','--processes',default=1, help="number of cpu's  to run in paralell, ROI <1000 will always use 1 core",type=int)
+#optArguments.add_argument('--tagKey',default=None, help="key for tag of interest",type=str)
+#optArguments.add_argument('--tagString',default=None, help="string contained in tag of interest",type=str)
 
 
 
@@ -32,11 +32,11 @@ class BinChrom():
         self.bam=bam
         self.binsize=binsize
         self.GetChromLenght()
-        self.tag=False
-        if tagKey is not None:
-            self.tag=True
-        self.tagKey=tagKey
-        self.tagOfInterest=tagOfInterest
+        #self.tag=False
+        #if tagKey is not None:
+    #        self.tag=True
+    #    self.tagKey=tagKey
+    #    self.tagOfInterest=tagOfInterest
         self.processes=processes
     def GetChromLenght(self):
         self.ChrLen={}
@@ -60,36 +60,50 @@ class BinChrom():
         return chunkss
 
     def FetchReads(self,chrom,start,end):
-        if not self.tag:
-            cov=0
-            with pysam.AlignmentFile(self.bam) as f:
-                for read in f.fetch(chrom,start,end):
-                    if not read.is_unmapped:
-                        cov=cov+read.rlen
-            cov=cov/(end-start)
-            return [chrom,start,end,cov]
+        cov=0
+        r=0
+        s=[]
+        e=[]
+        with pysam.AlignmentFile(self.bam) as f:
+            for read in f.fetch(chrom,start,end):
+                if not read.is_unmapped:
+                    #print(read)
+                    s.append(read.reference_start)
+                    e.append(read.reference_end)
+                    r=r+1
+        s=[x if x >= start else start for x in s ]
+        e=[x if x <= end else end for x in e ]
+        s=np.array(s)
+        e=np.array(e)
+        nts=sum(e-s)
+        cov=nts/(end-start)
+        return [chrom,start,end,r,cov]
 
-        else:
-            cov=0
-            with pysam.AlignmentFile(self.bam) as f:
-                for read in f.fetch(chrom,start,end):
-                    if not read.is_unmapped:
-                        x=[x for x in read.tags if self.tagKey in x]
-                        if len(x)<1:
-                            continue
-                        x=x[0][-1]
-
-                        if x==self.tagOfInterest:
-                            cov=cov+read.rlen
-            cov=cov/(end-start)
-            return [start,end,cov]
+    #    else:
+    #        cov=0
+    #        r=0
+    #        with pysam.AlignmentFile(self.bam) as f:
+    #            for read in f.fetch(chrom,start,end):
+    #                if not read.is_unmapped:
+    #                    x=[x for x in read.tags if self.tagKey in x]
+    #                    if len(x)<1:
+    #                        continue
+    #                    x=x[0][-1]
+#
+#                        if x==self.tagOfInterest:
+#                            r=r+1
+#                            cov=cov+read.rlen
+#            cov=cov/(end-start)
+#            return [start,end,cov,reads]
 
     def StartCalc(self):
         chunk=self.chunks()
-        print(chunk)
-        print('calc chunk')
+        results=[]
         with Pool(processes=self.processes) as pool:
-            results=pool.starmap(self.FetchReads,chunk)
+            jobs=[pool.apply_async(self.FetchReads,args=(i[0],i[1],i[2])) for i in chunk]
+
+            for job in tqdm(jobs):
+                results.append(job.get())
         return results
 
 
@@ -98,10 +112,12 @@ if __name__ == '__main__':
         parser.print_help(sys.stderr)
         sys.exit(1)
     args=parser.parse_args()
-    cc=BinChrom(args.bam,tagKey=args.tagKey,tagOfInterest=args.tagString,binsize=args.binsize,chrom=args.chromosome,processes=args.threads)
+    #cc=BinChrom(args.bam,tagKey=args.tagKey,tagOfInterest=args.tagString,binsize=args.binsize,chrom=args.chromosome,processes=args.threads)
+    cc=BinChrom(args.bam,binsize=args.binsize,chrom=args.chromosome,processes=args.processes)
+
     print('start multi')
     results=cc.StartCalc()
     with open(args.out,'w') as o:
-        o.write('contig,s,e,c\n')
+        o.write('contig,start,end,reads,coverage\n')
         for r in results:
-            o.write(f'{str(r[0])},{str(r[1])},{str(r[2])},{str(r[3])}\n')
+            o.write(f'{str(r[0])},{str(r[1])},{str(r[2])},{str(r[3])},{str(r[4])}\n')
